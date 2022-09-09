f49a49c0eaf337ba6b5263b4ca161b029be776d9
Marc Gonzalez <marcgonzaleztarrio@gmail.com>
2022-09-09 18:14:35 +0200

    java finish day 5

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 // Iteration 1: Names and Input
-//
+let hacker1 = "Bogdan";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Andreu";
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
-
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 // Iteration 3: Loops
+
+const reversedArray = hacker1.split("")
+console.log(reversedArray)
+const reversed = reversedArray.reverse()
+console.log("Joined reserved array", reversed.join(""));
